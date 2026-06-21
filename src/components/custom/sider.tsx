b16352878e51acd5b17eb87505329356f06c4238
wmgcat <developer@wmgcat.net>
2026-06-21 14:41:29 +0300

    update

--- src/components/custom/sider.tsx
+++ src/components/custom/sider.tsx
@@ -61,9 +61,14 @@ const Sider: React.FC<{
   return (
     <aside className={`sticky top-0 left-0 max-md:bottom-0 max-md:top-auto h-screen max-md:w-screen max-md:h-20 box-border px-4 py-2 ${collapse ? "w-28" : "min-w-50"} z-11`}>
       <div className="flex flex-col items-center gap-4 border border-br rounded-full h-full p-2 bg-back max-md:flex-row">
-        <div className="flex justify-center items-center max-w-12 max-h-16 w-full h-full">
-          <img src={collapse? imgLogo : imgLabel} className="flex justify-center items-center p-1 w-full aspect-square h-full z-1 pointer-events-none select-none" />
-        </div>
+        <NavLink
+          to={paths.about}
+          className="flex justify-center items-center"
+        >
+          <div className="flex justify-center items-center max-w-12 max-h-16 w-full h-full">
+            <img src={collapse? imgLogo : imgLabel} className="flex justify-center items-center p-1 w-full aspect-square h-full z-1 pointer-events-none select-none" />
+          </div>
+        </NavLink>
         <div className="h-full flex flex-col max-md:flex-row gap-4 overflow-y-auto max-md:overflow-x-auto no-scrollbar">
           <nav className="flex flex-col max-md:flex-row gap-4 flex-1">
             <NavLink
@@ -128,20 +133,6 @@ const Sider: React.FC<{
             <hr
               className="border-0 border-b border-b-br max-md:hidden"
             />
-            <NavLink
-              to={paths.about}
-              className="hover:bg-text/5 rounded-2xl transition-colors flex justify-center items-center"
-            >
-              {({ isActive }) => (
-                <Button
-                  variant="clear"
-                  className={`w-full justify-start ${isActive ? "text-primary bg-text/3" : "text-text"}`}
-                >
-                  <BiInfoCircle />
-                  {!collapse && t("footer.about")}
-                </Button>
-              )}
-            </NavLink>
             <NavLink
               to={paths.donut}
               className="hover:bg-text/5 rounded-2xl transition-colors flex justify-center items-center"
